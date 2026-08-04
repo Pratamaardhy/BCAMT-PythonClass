d085ff90300dd7bef5e67b25f51fd34ff60f02bd
root <root@LAPTOP-16OTS8J0.localdomain>
2026-08-04 10:20:31 +0700

    update file

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        String nama = "Ardi";
+        String alamat = "Jakarta Selatan";
+        int umur = 22;
+        Userprofile.showMyProfile(nama, alamat, umur);
+    }
+}
+
+
+/* public class Main { // hilangkan comment untuk menjalankan karena hanya satu file
+    public static void showMyProfile() {
+        String nama = "Gede Ardi Pratama";
+        String alamat = "Jakarta Selatan";
+        int umur = 22;
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Umur: " + umur);
+    }
+    public static void main(String[] args) {
+        showMyProfile();
+    }
+}*/
--- test java.java
+++
@@ -1,7 +0,0 @@
-// Nama class utama
-public class Main {  
-    public static void main(String[] args) {
-        // Mencetak teks ke konsol
-        System.out.println("Hello, World!");
-    }
-}
